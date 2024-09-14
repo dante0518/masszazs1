e57b8858016afaf9f3c718dbf9c6c46b3729abbe
dante0518 <dante0518@gmail.com>
2024-09-14 19:17:21 +0200

    updated/added buttons

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -10,9 +10,10 @@ function Hero() {
       <div className="hero-intro-container">
         <h1 className='header-h1'>Üdvözöljük a Bástya Masszázsnál</h1>
         <p className='hero-introduction'>Kiss Zoltán Károly vagyok, ex-katona és szenvedélyes masszőr. Katonai szolgálatom alatt kezdtem el masszírozni a táborokban, ahol gyorsan rájöttem, hogy mennyire élvezem ezt a tevékenységet. Számomra a masszázs nem csupán munka, hanem a kikapcsolódás és a gyógyítás eszköze.</p>
-      </div>
-        <img src={Kep} alt="Masszőr" id='hero-img' />
         <button className='button-reservation' id='reservation-480' onClick={handleClick}>Foglalás</button>
+      </div>
+      <img src={Kep} alt="Masszőr" id='hero-img' />
+
     </div>
   )
 }
